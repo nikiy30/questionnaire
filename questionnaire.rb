953aa2659605b82ab8fbd0ae7cd4ca0fca61f579
Nikita Vidal <n.vidal30@gmail.com>
2016-10-19 17:27:17 -0400

    Questions

---
+++ questionnaire.rb
@@ -0,0 +1,15 @@
+#Don't send dead people questionnaires!
+
+dead_people = ["Darth Vader","Ben","Han solo"]
+alive_people = ["Rey","bb-8","Luke"]
+
+name = "Han Solo"
+
+puts dead_people.include? name 
+puts alive_people.include? name
+
+if dead_people.include? name then
+	puts "Don't send a questionnaire to #{name}"
+elsif alive_people.include? name then 
+	puts "send questionnaire to #{name}"	
+end
